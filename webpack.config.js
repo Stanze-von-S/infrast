a7a6afde90440c80ef94caa033063ae3f3c84a70
Konstantin Mamonov <stanze.von.saidenberg@yandex.ru>
2020-02-13 23:07:41 +0300

    Modificate webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
 module.exports = {
     entry: {main: './src/code.js'},
     output:{
@@ -10,7 +11,14 @@ module.exports = {
             test: /\.js$/, // регулярное выражение, которое ищет все js файлы
             use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
             exclude: /node_modules/ // исключает папку node_modules
+                },
+                {
+            test: /\.css$/, // применять это правило только к CSS-файлам
+            use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
                 }
             ]
-        }
+        },
+    plugins: [ 
+        new MiniCssExtractPlugin({filename: 'style.css'})
+        ]
 }
