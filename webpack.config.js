4f1019ba985273ec3f3bdfce08d10c21af997c6a
Konstantin Mamonov <stanze.von.saidenberg@yandex.ru>
2020-02-15 18:46:23 +0300

    Modificate webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,10 @@ module.exports = {
                 }
                     ]
                 },
+                {
+                    test: /\.(eot|ttf|woff|woff2)$/,
+                    loader: 'file-loader?name=./vendor/[name].[ext]'
+                }
             ]
         },
     plugins: [ 
