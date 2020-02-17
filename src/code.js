6f0a21f56541d28a3c18d84b18eda8024a6b1529
Konstantin Mamonov <stanze.von.saidenberg@yandex.ru>
2020-02-17 06:25:04 +0300

    Modificate code.js

--- src/code.js
+++ src/code.js
@@ -1,9 +1,10 @@
 import './pages/index.css';
 import {Api, configApi} from './js/Api';
 import CardList from './js/CardList';
-import PopupForm from './js/PopupForm';
+import PopupFormEdit from './js/PopupFormEdit';
+import PopupFormPlace from './js/PopupFormPlace';
 import PopupImage from './js/PopupImage';
-import {validationForm, getProfile} from './js/functions';
+import {getProfile} from './js/functions';
 // import {validationInput, validationForm, getProfile} from './js/functions';
 
 // import PopupFormPlace from './js/PopupFormPlace';
@@ -15,59 +16,9 @@ import {validationForm, getProfile} from './js/functions';
 
 
 
-class PopupFormEdit extends PopupForm {
-    constructor(selector, obj) {
-        super(selector, obj);
-        // нельзя такое в конструкторе делать, присваивать значения 
-        this.container.querySelector('.popup__input_type_user').value = 'Jaques Causteau';
-        this.container.querySelector('.popup__input_type_job').value = 'Sailor, Researcher';
-        this.container.querySelector('.button_save').classList.add('popup__button_active');
-        this.form.user.addEventListener('input', () => this.functionInput(event));
-        this.form.job.addEventListener('input', () => this.functionInput(event));
-    }
 
-    functionForm(event, obj) {
-        event.preventDefault();
-        const form = event.target;
-        this.userName = this.container.querySelector('.popup__input_type_user').value;
-        this.jobName = this.container.querySelector('.popup__input_type_job').value;
-        obj.editProfileApi(this.userName, this.jobName);
-        profileName.textContent = this.userName;
-        profileJob.textContent = this.jobName;
-        form.reset();
-        validationForm(form.user);
-        this.close();
-    }
 
-}
 
-class PopupFormPlace extends PopupForm {
-    constructor(selector, obj) {
-        super(selector, obj);
-        // Надо исправить : Нельзя вызывать или создавать реализацию в конструторе класса
-        // Вызывая реализацию в конструторе класса, вы заведомо делаете класс не тестируемым.
-        // Такие класс нельзя правильно наследовать, а при вызове класса всегда будет вызываться реализация
-        this.form.name.addEventListener('input', () => this.functionInput(event));
-        this.form.link.addEventListener('input', () => this.functionInput(event));
-    }
-
-    functionForm(event, obj) {
-        event.preventDefault();
-        const form = event.target;
-        placesList.addCard(obj);
-        form.reset();
-        validationForm(form.name);
-        this.close();
-    }
-
-    getName() {
-        return this.form.name.value;
-    }
-
-    getLink() {
-        return this.form.link.value;
-    }
-}
 
 
 
